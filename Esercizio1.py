b21c1892b77aae14b36d189477176e73223bcb30
SteppoC <122269089+SteppoC@users.noreply.github.com>
2023-01-10 10:12:55 +0000

    modi 1,2,3

--- Esercizio1.py
+++ Esercizio1.py
@@ -1,3 +1,27 @@
 lista=[1,4,67,59]
+
+print('--- MODO 1 ---')
 a=sum(lista)
 print('La somma della lista è: {}' .format(a))
+
+print('--- MODO 2 ---')
+def somma(list):
+    Sum=0;
+    for i in range(len(list)):
+        Sum=list[i]+Sum
+    return Sum
+risult=somma(lista)
+print('La somma della lista è: {}' .format(risult))
+
+print('--- MODO 3 ---')
+def somma(list):
+    Sum=0;
+    for element in list:
+        Sum=Sum+element
+    return Sum
+risult=somma(lista)
+print('La somma della lista è: {}' .format(risult))
+print()
+fruits = ["apple", "banana", "cherry"]
+for x in fruits:
+    print(x)
